01928b15b5434a0b01acc6fcacd1a9fed423c8e7
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-05-20 21:39:42 -0300

    Commit

--- Exercicio_1/App.js
+++ Exercicio_1/App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
 
 export default function App() {
-  const [n1, setN1] = useState ('');
-  const [n2, setN2] = useState ('');
-  const [n3, setN3] = useState ('');
+  const [N1, setN1] = useState ('');
+  const [N2, setN2] = useState ('');
+  const [N3, setN3] = useState ('');
   const [media, setMedia] = useState ('');
   return (
     <View style={styles.container}>
       <Text style={styles.label} >Nota 1</Text>
       <TextInput style={styles.input}
-        on
+        keyboardType='numeric'
+        value={N1}
+        onChangeText={setN1}
       />
       <Text style={styles.label} >Nota 2</Text>
+      <TextInput style={styles.input}
+        keyboardType='numeric'
+        value={N2}
+        onChangeText={setN2}
+      />
       <Text style={styles.label} >Nota 3</Text>
+      <TextInput style={styles.input}
+        keyboardType='numeric'
+        value={N3}
+        onChangeText={setN3}
+      />
+
+      <Button 
+      title='Calcular'
+        onPress={() => {
+          const note_1 = parseFloat (N1);
+          const note_2 = parseFloat(N2);
+          const note_3 = parseFloat(N3);
+          if (!isNaN(N1) && !isNaN (N2)&& !isNaN (N3)) {
+            setMedia ((note_1 + note_2 + note_3)/3);
+          } else {
+            setMedia ('Numero Invalido');
+          } 
+        }
+        }
+      />
+
+      {media!== null && (
+
+        <Text style={styles.media} >Media: {media}</Text>
+      )  
+    }
 
       <StatusBar style="auto" />
     </View>
