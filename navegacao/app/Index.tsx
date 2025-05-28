502ef072f788a704c8c30db3f7a6747b37204c6c
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-05-27 21:44:55 -0300

    Commit

---
+++ navegacao/app/Index.tsx
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Label from '../components/Label'; 
+import Botao from '../components/Botao'; 
+
+
+const Index = () => {
+  return (
+    <ImageBackground source={require('../assets/Telafundo.jpg')}
+      style={styles.imagem}>
+
+        <View style={styles.styleLabel}>
+          <Label Label = 'Home'/>
+        </View>
+        <View style={styles.styleBotao}>
+          <Label Botao = 'Buscar usuario'/>
+        </View>
+      <StatusBar style="auto" />
+    </ImageBackground>
+
+  );
+};
+
+export default Index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imagem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  styleLabel : {
+
+  },
+  styleBotao :{
+
+  }});
