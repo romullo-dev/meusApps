0214967664ed2574e9fa6b9bee1030db22eb4046
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-06-09 21:34:54 -0300

    c

--- Atividade_Avaliativa_3/app/Lista.js
+++ Atividade_Avaliativa_3/app/Lista.js
@@ -1,22 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button ,ScrollView} from 'react-native';
 import { useState } from 'react';
 
-const Lista = () => {
-    const [lista, setLista] = useState();
+const Lista = (navigation) => {
+  const [lista, setLista] = useState([]);
+  const [itens, setItens] = useState();
+
+  const adicionarItem = () => {
+    
+     setLista ([...lista, itens]);
+    setItens ('');
+  }; 
+
   return (
     <View style={styles.container}>
       <TextInput style={styles.input}
-        placeholder='Itens'
-        onChangeText={setLista}
-        value='lista'
+        placeholder='Itenaaaaaaaaaaaaaaas'
+        onChangeText={setItens}
+        value={itens}
       />
 
       <Button style={styles.container}
         title='+'
-        onPress={() = }
+        onPress={adicionarItem}
       />
+
+
+      <ScrollView style={styles.lista}>
+        {lista.map((item, index) => (
+          <Text key={index} style={styles.item}>
+            • {item}
+          </Text>
+        ))}
+      </ScrollView>
+
+
+      <Button 
+            title='Sair'
+            onPress={() => navigation.navigate('Home') }
+        />
+
     </View>
+
+    
   );
 }
 
