1db25cf380c3f38943c63c302a13e2331b5f17c7
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-06-13 20:08:11 -0300

    Commit

--- Stack-Navigator-Exercicio/app/home.js
+++ Stack-Navigator-Exercicio/app/home.js
@@ -7,7 +7,9 @@ import { useState } from 'react';
 const Home = () => {
     const [coca, setCoca ] = useState(0);
     const [batata, setBatata ] = useState(0);
-    const [hamg, setHamg ] = useState(0 );
+    const [hamg, setHamg ] = useState(0);
+    const [total, seTotal] = useState(0);
+
 
     return (
         <View style={styles.container}>
@@ -19,31 +21,37 @@ const Home = () => {
                     <Text style={styles.botaoTexto}>+</Text>
                 </TouchableOpacity>
                 <Text>{coca}</Text>
-                <TouchableOpacity style={styles.botao}>
+                <TouchableOpacity style={styles.botao} onPress={()=> setCoca(coca-1)}>
                     <Text style={styles.botaoTexto}>-</Text>
                 </TouchableOpacity>
             </View>
             <View style={styles.label}>
                 <Image source={{ uri: 'https://okdiario.com/img/recetas/2016/08/30/patatas-fritas-1.jpg' }}
                     style={{ width: 90, height: 90, borderRadius: 15 }} />
-                <TouchableOpacity style={styles.botao}>
+                <TouchableOpacity style={styles.botao} onPress={()=> setBatata(batata+1)} >
                     <Text style={styles.botaoTexto}>+</Text>
                 </TouchableOpacity>
-                <Text>1</Text>
-                <TouchableOpacity style={styles.botao}>
+                <Text>{batata}</Text>
+                <TouchableOpacity style={styles.botao} onPress={()=> setBatata(batata-1)} >
                     <Text style={styles.botaoTexto}>-</Text>
                 </TouchableOpacity>
             </View>
             <View style={styles.label}>
                 <Image source={{ uri: 'https://th.bing.com/th/id/R.8946e10a2dc06335176e66a895ebc187?rik=HkaV4oVqHS7bHQ&riu=http%3a%2f%2fimages.media-allrecipes.com%2fuserphotos%2f960x960%2f3757723.jpg&ehk=aFAF%2buIjH1v%2fJNECcIK41A8z255yDgsvBlm4u2pjzNE%3d&risl=&pid=ImgRaw&r=0' }}
                     style={{ width: 90, height: 90, borderRadius: 15 }} />
-                <TouchableOpacity style={styles.botao}>
+                <TouchableOpacity style={styles.botao}onPress={()=> setHamg(hamg+1)}>
                     <Text style={styles.botaoTexto}>+</Text>
                 </TouchableOpacity>
-                <Text>1</Text>
-                <TouchableOpacity style={styles.botao}>
+                <Text>{hamg}</Text>
+                <TouchableOpacity style={styles.botao} onPress={()=> setHamg(hamg-1)}>
                     <Text style={styles.botaoTexto}>-</Text>
                 </TouchableOpacity>
+            </View>
+
+            <View style={styles.totalView}>
+                <Text>R$ </Text>
+
+
             </View>
 
             
@@ -80,5 +88,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         gap: 25,
         padding: 10,
+    },
+    totalView :{
+        backgroundColor: '#ADD8E6',
     },
 });
