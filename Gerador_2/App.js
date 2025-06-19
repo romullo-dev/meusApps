fb62a4810c35f236bf7f18fc0190b4a6e66ca773
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-06-19 17:33:44 -0300

    comde

--- Gerador_2/App.js
+++ Gerador_2/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import home from "./app/home";
 import Senhas from "./app/Senhas";
+import { Ionicons } from '@expo/vector-icons';
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
@@ -10,7 +11,24 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+    screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Senhas') {
+              iconName = focused ? 'lock-closed' : 'lock-closed-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#392de9',
+          tabBarInactiveTintColor: 'gray',
+        })}
+    
+    >
       <Tab.Navigator>
         <Tab.Screen name="home" component={home} />
         <Tab.Screen name="Senhas" component={Senhas} />
